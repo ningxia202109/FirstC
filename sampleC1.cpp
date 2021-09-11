980fd4b2d59fd91d55a5495bc31a5df2c610f8ce
Ning Xia <90520833+ningxia202109@users.noreply.github.com>
2021-09-11 19:02:40 +0000

    rename to sampleC1

--- sampleC1.cpp
+++ sampleC1.cpp
@@ -3,6 +3,11 @@
 
 using namespace std;
 
+struct dog {
+  int age;
+  int weight;
+};
+
 int main() {
   int numbers[5] = {1,2,3,4,5};
   // for loop 1
@@ -13,5 +18,10 @@ int main() {
   for (int i:numbers) {
     printf("%d\n", i);
   }
+  // struct 1
+  struct dog oneDog;
+  oneDog.age = 2;
+  oneDog.weight = 10;
+  printf("%d", oneDog.age);
   return 0;
 }
