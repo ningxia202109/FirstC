58de1ec85ff54120ddabfd0d8d30f0a904720cfc
Ning Xia <90520833+ningxia202109@users.noreply.github.com>
2021-09-11 18:40:44 +0000

    add for loop examples

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <stdio.h>
+
+using namespace std;
 
 int main() {
-  // little update
-  std::cout << "Hello World!\n";
-
+  int numbers[5] = {1,2,3,4,5};
+  // for loop 1
+  for (int i = 0; i < 5; i++) {
+    printf("%d\n", numbers[i]);
+  }
+  // for loop 2
+  for (int i:numbers) {
+    printf("%d\n", i);
+  }
+  return 0;
 }
